a24428f070ae81085f61338e570103395f718160
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-05-21 23:52:10 +0900

    add entity

---
+++ src/main/java/com/example/simple/entity/Book.java
@@ -0,0 +1,21 @@
+package com.example.simple.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(nullable = false, unique = true)
+	private String title;
+
+	@Column(nullable = false)
+	private String author;
+}
